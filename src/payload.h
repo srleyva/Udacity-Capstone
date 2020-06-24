d377fbd0b1dab29d30543d3f115cbdb7a6bfc69a
Stephen Leyva <sleyva1297@gmail.com>
2020-06-23 20:34:06 -0700

    Prepare ICMP packet, switch to unique_ptrs and implement TODO packet to prevent nil pointers

--- src/payload.h
+++ src/payload.h
@@ -1,8 +1,29 @@
 #ifndef PAYLOAD_H
 #define PAYLOAD_H
 
-class PacketPayload {
-    void virtual Prepare() = 0;
+#include <string>
+#include <vector>
+
+class Payload {
+public:
+    virtual std::unique_ptr<Payload> Handle() = 0;
+    virtual std::string String() = 0;
+};
+
+class TODOPayload : public Payload {
+public:
+    TODOPayload(int proto) : _proto(proto) {}
+    std::unique_ptr<Payload> Handle() {
+        return std::make_unique<TODOPayload>(this->_proto);
+    }
+
+    std::string String() {
+        std::stringstream output;
+        output << "Proto not implemented: " << this->_proto;
+        return output.str();
+    }
+private:
+    int _proto;
 };
 
 #endif
